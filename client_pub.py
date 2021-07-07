85ce8576f9330c379fb6998f24e2e8810c064a5d
Sanjay Maregouda <msanjay888@gmail.com>
2021-07-07 23:32:01 +0530

    Initial Commit

---
+++ client_pub.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import time
+
+# This is the Publisher
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+
+
+client = mqtt.Client()
+client.connect("localhost",1883,60)
+
+client.on_connect = on_connect
+
+client.publish("topic/test", "Hello world!")
+time.sleep(2)
+client.disconnect()
